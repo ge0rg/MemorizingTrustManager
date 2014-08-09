6456c61ea8bba1111611eb401de1b7fd510cfae9
Florian Schmaus <flo@geekplace.eu>
2014-08-09 14:57:47 +0200

    Remove unused import

--- example/src/de/duenndns/mtmexample/MTMExample.java
+++ example/src/de/duenndns/mtmexample/MTMExample.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
